51e05193d6095978f483295aeb21709b37dacbd9
Élio Cró <elio@front.no>
2017-12-15 15:43:29 +0000

    Refactor to use promises throughout code

--- flexwin.js
+++ flexwin.js
@@ -1,19 +1,16 @@
 'use strict';
-
-/**
+/*
  * A wrapper for the DIBS Payment Services Flexwin API
  */
 
-var q = require('q');
-var request = require('request');
-
-request.defaults({
+require('request').defaults({
   jar: false,
   pool: false
 });
+const request = require('request-promise-native');
+
 
 module.exports = {
-
   /*
    *  Sets a global test mode
   */
@@ -28,33 +25,33 @@ module.exports = {
    *  This service performs a credit and debit card check and saves the credit card
    *  information for recurring payments.
   */
-  createTicket: function(options) {
+  createTicket (options) {
     return this.dibsRequest(options, this.createTicketUri);
   },
 
-  /**
+  /*
    *  Endpoint for ticket authorizations
   */
   authorizeTicketUri: 'https://payment.architrade.com/cgi-ssl/ticket_auth.cgi',
 
-  /**
+  /*
    *  Make a recurring payment using a ticket previously created via the
    *  createTicket service.
   */
-  authorizeTicket: function(options) {
+  authorizeTicket (options) {
     return this.dibsRequest(options, this.authorizeTicketUri);
   },
 
-  /**
+  /*
    *  Endpoint for capturing transactions
   */
   captureTransactionUri: 'https://payment.architrade.com/cgi-bin/capture.cgi',
 
-  /**
+  /*
    *  The second part of any transaction is the capture process. Usually this take place
    *  at the time of shipping the goods to the customer.
   */
-  captureTransaction: function(options) {
+  captureTransaction (options) {
     return this.dibsRequest(options, this.captureTransactionUri);
   },
 
@@ -62,43 +59,28 @@ module.exports = {
    *  Executes the https request to the DIBS server and fulfills the promise
    *  with the response JSON Object
   */
-  dibsRequest: function(options, uri) {
-    if (this.testMode) {
+  async dibsRequest (options, uri) {
+    if(this.testMode) {
       options.test = 'yes';
     }
     options.textreply = 'yes';
     options.fullreply = 'yes';
 
-    var self = this;
-    var d = q.defer();
-
-    request.post({
+    const body = await request.post({
       uri: uri,
       form: options
-    },
-    function(err, res, body) {
-      if (err) {
-        return d.reject(err);
-      }
-      try {
-        d.resolve(self.parseDibsResponse(body));
-      }
-      catch (err) {
-        d.reject(err);
-      }
     });
-
-    return d.promise;
+    return this.parseDibsResponse(body);
   },
 
   /*
    * Converts the DIBS text reply to a javascript object
   */
-  parseDibsResponse: function(str) {
-    var parts = str.split('&');
-    var res = {};
-    for(var i = 0, l = parts.length; i<l; i++) {
-      var item = parts[i].split('=');
+  parseDibsResponse (str) {
+    const parts = str.split('&');
+    const res = {};
+    for(let i = 0, l = parts.length; i < l; i++) {
+      const item = parts[i].split('=');
       res[item[0]] = decodeURIComponent(item[1]);
     }
     return res;
